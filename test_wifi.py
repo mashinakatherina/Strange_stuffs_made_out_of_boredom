763e1857161f91fd88c75e2b95795a4c02aed121
Katerina Mashina <mashina.katherina@gmail.com>
2020-05-17 17:51:15 +0300

    script for searching for wifi
    Searching for networks available for connection

---
+++ test_wifi.py
@@ -0,0 +1,18 @@
+import subprocess
+
+results = subprocess.check_output(["netsh", "wlan", "show", "network"])
+results = results.decode("ascii", "ignore")
+results = results.replace("\r", "")
+ls = results.split("\n")
+ls = ls[4:]
+ssids = []
+i = 0
+while i < len(ls):
+    if i % 5 == 0:
+        ssids.append(ls[i])
+    i += 1
+
+i = 0
+while i < len(ssids):
+    print(ssids[i])
+    i += 1
